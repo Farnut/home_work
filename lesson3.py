8e4aa9168bae95ac64094306830a0dbf51df2c15
Farnut <90102580+Farnut@users.noreply.github.com>
2022-01-13 20:34:00 +0200

    Initial commit

---
+++ lesson3.py
@@ -0,0 +1,30 @@
+##lessons3
+#if\elif\else
+
+# print(2+2)
+# print(2/4)
+# print(2<4)
+# print(4<=4)
+# print(5<2)
+
+# age = 13
+# if age < 18:
+#     print('child')
+# else:
+#     print('adult')
+
+# weight = 150
+# if weight <= 150:
+#     print(weight)
+# else:
+#     print("Too much")
+
+# weight = 0.11
+# if weight < 0:
+#     print("error-")
+# elif weight <= 0.1:
+#     print('too small')
+# elif weight > 150:
+#     print('too much')
+# else:
+#     print(weight)
