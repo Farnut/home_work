d088c028a03a2e055132828a38869f5b827a6221
Farnut <90102580+Farnut@users.noreply.github.com>
2022-01-31 22:45:06 +0200

    hw_01_02

--- hw_01_02.py
+++ hw_01_02.py
@@ -30,49 +30,49 @@
 Зарплата менеджера составляет 200$ + процент от продаж, продажи до 500$ — 3%, от 500 до 1000 — 5%, свыше 1000 — 8%. Пользователь 
 вводит с клавиатуры уровень продаж для трех менеджеров. Определить их зарплату, определить лучшего менеджера, начислить ему премию
 200$, вывести итоги на экран.'''
-stock = 200
-bonus = 200
-percnt_1 = 0.03
-percnt_2 = 0.05
-percnt_3 = 0.08
-man1 = "Manager 1"
-man2 = "Manager 2"
-man3 = "Manager 3"
-man1_salary = 0
-man2_salary = 0
-man3_salary = 0
-man_sale_1 = float(input(f"Enter Manager 1 sales : "))
-man_sale_2 = float(input(f"Enter Manager 2 sales : "))
-man_sale_3 = float(input(f"Enter Manager 3 sales : "))
-if man_sale_1 < 500:
-    man1_salary = stock + (man_sale_1 * percnt_1)
-elif man_sale_1 >= 500 and man_sale_1 <= 1000:
-    man1_salary = stock + (man_sale_1 * percnt_2)
-elif man_sale_1 > 1000:
-    man1_salary = stock + (man_sale_1 * percnt_3)
-if man_sale_2 < 500:
-    man2_salary = stock + (man_sale_2 * percnt_1)
-elif man_sale_2 >= 500 and man_sale_2 <= 1000:
-    man2_salary = stock + (man_sale_2 * percnt_2)
-elif man_sale_2 > 1000:
-    man2_salary = stock + (man_sale_2 * percnt_3)
-if man_sale_3 < 500:
-    man3_salary = stock + (man_sale_3 * percnt_1)
-elif man_sale_3 >= 500 and man_sale_3 <= 1000:
-    man3_salary = stock + (man_sale_3 * percnt_2)
-elif man_sale_3 > 1000:
-    man3_salary = stock + (man_sale_3 * percnt_3)
-top = max(man_sale_1, man_sale_2, man_sale_3)
-if top == man_sale_1:
-    man1_salary = man1_salary + 200
-    print(f"TOP is {man1} He gets a {bonus}$ bonus.")
-elif top == man_sale_2:
-    man2_salary = man2_salary + 200
-    print(f"TOP is {man2} He gets a {bonus}$ bonus.")
-elif top == man_sale_3:
-    man3_salary = man3_salary + 200
-    print(f"TOP is {man3}. He gets a {bonus}$ bonus.")
-print(f"{man1} salary is {man1_salary}$")
-print(f"{man2} salary is {man2_salary}$")
-print(f"{man3} salary is {man3_salary}$")
+# stock = 200
+# bonus = 200
+# percnt_1 = 0.03
+# percnt_2 = 0.05
+# percnt_3 = 0.08
+# man1 = "Manager 1"
+# man2 = "Manager 2"
+# man3 = "Manager 3"
+# man1_salary = 0
+# man2_salary = 0
+# man3_salary = 0
+# man_sale_1 = float(input(f"Enter Manager 1 sales : "))
+# man_sale_2 = float(input(f"Enter Manager 2 sales : "))
+# man_sale_3 = float(input(f"Enter Manager 3 sales : "))
+# if man_sale_1 < 500:
+#     man1_salary = stock + (man_sale_1 * percnt_1)
+# elif man_sale_1 >= 500 and man_sale_1 <= 1000:
+#     man1_salary = stock + (man_sale_1 * percnt_2)
+# elif man_sale_1 > 1000:
+#     man1_salary = stock + (man_sale_1 * percnt_3)
+# if man_sale_2 < 500:
+#     man2_salary = stock + (man_sale_2 * percnt_1)
+# elif man_sale_2 >= 500 and man_sale_2 <= 1000:
+#     man2_salary = stock + (man_sale_2 * percnt_2)
+# elif man_sale_2 > 1000:
+#     man2_salary = stock + (man_sale_2 * percnt_3)
+# if man_sale_3 < 500:
+#     man3_salary = stock + (man_sale_3 * percnt_1)
+# elif man_sale_3 >= 500 and man_sale_3 <= 1000:
+#     man3_salary = stock + (man_sale_3 * percnt_2)
+# elif man_sale_3 > 1000:
+#     man3_salary = stock + (man_sale_3 * percnt_3)
+# top = max(man_sale_1, man_sale_2, man_sale_3)
+# if top == man_sale_1:
+#     man1_salary = man1_salary + 200
+#     print(f"TOP is {man1} He gets a {bonus}$ bonus.")
+# elif top == man_sale_2:
+#     man2_salary = man2_salary + 200
+#     print(f"TOP is {man2} He gets a {bonus}$ bonus.")
+# elif top == man_sale_3:
+#     man3_salary = man3_salary + 200
+#     print(f"TOP is {man3}. He gets a {bonus}$ bonus.")
+# print(f"{man1} salary is {man1_salary}$")
+# print(f"{man2} salary is {man2_salary}$")
+# print(f"{man3} salary is {man3_salary}$")
 
